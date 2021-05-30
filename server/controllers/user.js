c4e81f1f5c88701ac5042bcc53752a0a9662a4be
DmitryRybalko <dmitryrybalkodev@gmail.com>
2021-05-30 22:31:52 +0800

    handle orders server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const Product = require("../models/product");
 const Cart = require("../models/cart");
+const Order = require("../models/order");
 
 exports.userCart = async (req, res) => {
   console.log(req.body);
@@ -50,6 +51,13 @@ exports.getUserCart = async (req, res) => {
   res.json({ products, cartTotal });
 };
 
+exports.emptyCart = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  const cart = await Cart.findOneAndRemove({ orderedBy: user._id }).exec();
+  res.json(cart);
+};
+
 exports.saveAddress = async (req, res) => {
   try {
     console.log(req.body);
@@ -63,4 +71,41 @@ exports.saveAddress = async (req, res) => {
     res.status(400).send("Save address failed");
     console.log(error);
   }
+};
+
+exports.createOrder = async (req, res) => {
+  const { paymentIntent } = req.body.stripeResponse;
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  let { products } = await Cart.findOne({ orderedBy: user._id }).exec();
+
+  let newOrder = await new Order({
+    products,
+    paymentIntent,
+    orderedBy: user._id,
+  }).save();
+
+  let bulkOption = products.map((item) => {
+    return {
+      updateOne: {
+        filter: { _id: item.product._id },
+        update: { $inc: { quantity: -item.count, sold: +item.count } },
+      },
+    };
+  });
+
+  let updated = await Product.bulkWrite(bulkOption, {});
+  console.log("product quantity-- and sold++", updated);
+  console.log("New order saved", newOrder);
+  res.json({ ok: true });
+};
+
+exports.orders = async (req, res) => {
+  let user = await User.findOne({ email: req.user.email }).exec();
+
+  let userOrders = await Order.find({ orderedBy: user._id })
+    .populate("products.product")
+    .exec();
+
+  res.json(userOrders);
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,10 +4,21 @@ const router = express.Router();
 
 const { authCheck } = require("../middlewares/auth");
 
-const { userCart, getUserCart, saveAddress } = require("../controllers/user");
+const {
+  userCart,
+  getUserCart,
+  saveAddress,
+  emptyCart,
+  createOrder,
+  orders,
+} = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart);
 router.get("/user/cart", authCheck, getUserCart);
+router.get("/user/orders", authCheck, orders);
 router.post("/user/address", authCheck, saveAddress);
+router.delete("/user/cart", authCheck, emptyCart);
+
+router.post("/user/order", authCheck, createOrder);
 
 module.exports = router;
