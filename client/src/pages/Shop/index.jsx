a7cb6c58b4a91fda96ce2cd742e2dddcb89564ca
DmitryRybalko <dmitryrybalkodev@gmail.com>
2021-05-31 21:00:45 +0800

    add loading spinner to the shop page

--- client/src/pages/Shop/index.jsx
+++ client/src/pages/Shop/index.jsx
@@ -4,18 +4,23 @@ import ProductCard from "../../components/Card/ProductCard";
 import { useEffect, useState } from "react";
 import { getProductsPagination } from "../../util/product";
 import HeroImage from "../../assets/contact/hero-image.jpg";
+import LoadingSpinner from "../../components/LoadingSpinner";
 import "./shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const loadAllProducts = () => {
+    setLoading(true);
     getProductsPagination("createdAt", "desc", page)
       .then((res) => {
         setProducts(res.data);
+        setLoading(false);
       })
       .catch((error) => {
+        setLoading(false);
         console.log(error);
       });
   };
@@ -28,9 +33,11 @@ const Shop = () => {
   const nextPage = (direction) => {
     if (direction === "Next" && products.length > 5) {
       setPage(page + 1);
+      setLoading(!loading);
       window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     } else if (direction === "Prev" && page > 1) {
       setPage(page - 1);
+      setLoading(!loading);
       window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     }
   };
@@ -42,19 +49,31 @@ const Shop = () => {
           image={HeroImage}
           header={"Take your adventure to the next level."}
         />
-        <div className="products-container">
-          {products.map((product) => (
-            <ProductCard product={product} key={product._id} />
-          ))}
-        </div>
-        <div className="pagination">
-          <button onClick={() => nextPage("Prev")} className="pagination__btn">
-            Prev
-          </button>
-          <button onClick={() => nextPage("Next")} className="pagination__btn">
-            Next
-          </button>
-        </div>
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <div className="products-container">
+              {products.map((product) => (
+                <ProductCard product={product} key={product._id} />
+              ))}
+            </div>
+            <div className="pagination">
+              <button
+                onClick={() => nextPage("Prev")}
+                className="pagination__btn"
+              >
+                Prev
+              </button>
+              <button
+                onClick={() => nextPage("Next")}
+                className="pagination__btn"
+              >
+                Next
+              </button>
+            </div>
+          </>
+        )}
       </div>
       <Footer />
     </>
